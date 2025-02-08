e98c0ebe8f6f7423e632ac264bf8c22935c0ed55
John Benedict Yan <johnbenedictyan@gmail.com>
2025-02-08 23:09:40 +0800

    format layout component

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,9 +1,8 @@
-import React from 'react'
-import Footer from './Footer';
-import Header from './Header';
+import React from "react";
 import { Helmet } from "react-helmet";
-import  { Toaster } from 'react-hot-toast';
-
+import { Toaster } from "react-hot-toast";
+import Footer from "./Footer";
+import Header from "./Header";
 
 const Layout = ({ children, title, description, keywords, author }) => {
   return (

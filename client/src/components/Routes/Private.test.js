ef061924523d99e06371591c494fb525b07596ce
John Benedict Yan <johnbenedictyan@gmail.com>
2025-02-08 23:07:20 +0800

    scaffold private route testcases

---
+++ client/src/components/Routes/Private.test.js
@@ -0,0 +1,11 @@
+describe("Private Route Component", () => {
+  describe("when user is logged in", () => {
+    it("should allow the user to access to child page", () => {});
+  });
+
+  describe("when user is not logged in", () => {
+    it("should redirect the user to the login page", () => {});
+  });
+
+  it("should render the loading spinner correctly", () => {});
+});

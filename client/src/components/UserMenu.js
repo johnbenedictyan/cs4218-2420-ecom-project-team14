02671c17255c5bef5a102ba06c7d4899d9d6460d
John Benedict Yan <johnbenedictyan@gmail.com>
2025-02-08 20:47:16 +0800

    format user menu file

--- client/src/components/UserMenu.js
+++ client/src/components/UserMenu.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+
 const UserMenu = () => {
   return (
     <div>
@@ -17,7 +18,7 @@ const UserMenu = () => {
             className="list-group-item list-group-item-action"
           >
             Orders
-          </NavLink>         
+          </NavLink>
         </div>
       </div>
     </div>

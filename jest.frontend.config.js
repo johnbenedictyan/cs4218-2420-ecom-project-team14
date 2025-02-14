99a36ee64708dcafdedf118ef32ea1c81d6184c4
jwdavis0200 <jwdavis0200@gmail.com>
2025-02-14 13:14:14 +0800

    update jest.frontend.config.js to include all js files outside Auth

--- jest.frontend.config.js
+++ jest.frontend.config.js
@@ -20,11 +20,14 @@ export default {
   transformIgnorePatterns: ["/node_modules/(?!(styleMock\\.js)$)"],
 
   // only run these tests
-  testMatch: ["<rootDir>/client/src/pages/Auth/*.test.js"],
+  testMatch: ["<rootDir>/client/src/pages/**/*.test.js"],
 
   // jest code coverage
   collectCoverage: true,
-  collectCoverageFrom: ["client/src/pages/Auth/**"],
+  collectCoverageFrom: [
+    "client/src/pages/**/*.js",
+    "!client/src/pages/**/*.test.js",
+  ],
   coverageThreshold: {
     global: {
       lines: 100,

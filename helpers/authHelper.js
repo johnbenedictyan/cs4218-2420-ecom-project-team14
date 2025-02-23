209be2e18ac2d1e28ac871d6059411584d65e38a
John Benedict Yan <johnbenedictyan@gmail.com>
2025-02-23 21:00:57 +0800

    add auth helpers test cases

--- helpers/authHelper.js
+++ helpers/authHelper.js
@@ -1,16 +1,20 @@
 import bcrypt from "bcrypt";
 
 export const hashPassword = async (password) => {
-    try {
-        const saltRounds = 10;
-        const hashedPassword = await bcrypt.hash(password, saltRounds);
-        return hashedPassword;
+  if (!password) return undefined;
 
-    } catch (error) {
-        console.log(error);
-    }
+  const trimmedPassword = password.trim();
+  if (!trimmedPassword) return undefined;
+
+  try {
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(trimmedPassword, saltRounds);
+    return hashedPassword;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-export const comparePassword = async (password,hashedPassword) => {
-    return bcrypt.compare(password, hashedPassword);
-}
+export const comparePassword = async (password, hashedPassword) => {
+  return bcrypt.compare(password, hashedPassword);
+};

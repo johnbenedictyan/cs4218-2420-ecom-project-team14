3e52436ac81eebf6b2d9a0d1701493f14a7f6bae
Sampy147 <samuelpangc@gmail.com>
2025-02-25 13:59:11 +0800

    Changed ids of mockproducts to Mongo's ObjectId type

--- controllers/tests/getProductController.test.js
+++ controllers/tests/getProductController.test.js
@@ -1,7 +1,7 @@
 import { jest } from "@jest/globals";
 import productModel from "../../models/productModel.js";
 import { getProductController } from "../productController.js";
-import { PRODUCT_LIMIT } from "../constants/productConstants.js";
+import { ObjectId } from "mongodb";
 
 jest.mock("../../models/productModel");
 describe("Get Product Controller tests", () => {
@@ -29,13 +29,13 @@ describe("Get Product Controller tests", () => {
 
   const mockProducts = [
     {
-      _id: "123",
+      _id: new ObjectId("242ddde9effa794b93f20688"),
       name: "Toy Giraffe",
       slug: "Toy-Giraffe",
       description: "Some toy giraffe",
       price: 10,
       category: {
-        _id: "456",
+        _id: new ObjectId("bc7f29ed898fefd6a5f713fd"),
         name: "Toys",
         slug: "toys",
         __v: 0,
@@ -46,13 +46,13 @@ describe("Get Product Controller tests", () => {
       __v: 0,
     },
     {
-      _id: "234",
+      _id: new ObjectId("51f45420a784984f2942e609"),
       name: "Toy cars",
       slug: "Toy-cars",
       description: "some toy cars",
       price: 15,
       category: {
-        _id: "456",
+        _id: new ObjectId("bc7f29ed898fefd6a5f713fd"),
         name: "Toys",
         slug: "toys",
         __v: 0,

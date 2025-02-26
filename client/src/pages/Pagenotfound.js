80b91a44b68ab778fc32f34cb20abdbd86ba5d28
jwdavis0200 <jwdavis0200@gmail.com>
2025-02-26 14:29:09 +0800

    improve SEO readability of pagenotfound

--- client/src/pages/Pagenotfound.js
+++ client/src/pages/Pagenotfound.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import Layout from "./../components/Layout";
 
 const Pagenotfound = () => {
+  // Update document title directly for SEO purposes when component is rendered
+  useEffect(() => {
+    const originalTitle = document.title;
+
+    document.title = "404 - Page Not Found | Ecommerce App";
+
+    // Cleanup function for when component unmounts by resetting document title
+    return () => {
+      document.title = originalTitle;
+    };
+  }, []);
+
   return (
     <Layout title={"go back- page not found"}>
       {/* Accessibility feature for assitive devices to show user 404 when page not found. */}

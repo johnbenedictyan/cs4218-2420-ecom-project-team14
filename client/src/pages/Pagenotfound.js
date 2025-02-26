6fd0207f61af91360fb8dbe2432e7fff554ac13c
jwdavis0200 <jwdavis0200@gmail.com>
2025-02-26 13:34:45 +0800

    improve accessibility feature of Pagenotfound.js

--- client/src/pages/Pagenotfound.js
+++ client/src/pages/Pagenotfound.js
@@ -5,8 +5,11 @@ import Layout from "./../components/Layout";
 const Pagenotfound = () => {
   return (
     <Layout title={"go back- page not found"}>
-      <div className="pnf">
-        <h1 className="pnf-title">404</h1>
+      {/* Accessibility feature for assitive devices to show user 404 when page not found. */}
+      <div className="pnf" role="alert" aria-labelledby="pnf-heading">
+        <h1 id="error-title" className="pnf-title">
+          404
+        </h1>
         <h2 className="pnf-heading">Oops ! Page Not Found</h2>
         <Link to="/" className="pnf-btn">
           Go Back

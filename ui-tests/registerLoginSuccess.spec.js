6d77c6e09cda9339a9025dc343e3729a1739e223
Douglas Lim <douglas.lim.yy@gmail.com>
2025-03-15 01:35:00 +0800

    Add admin login case for ui test

--- ui-tests/registerLoginSuccess.spec.js
+++ ui-tests/registerLoginSuccess.spec.js
@@ -5,6 +5,8 @@ import userModel from '../models/userModel';
 
 dotenv.config();
 
+let emailForDeletion;
+
 test.beforeEach(async ({ page }) => {
     // Connecting to db which is needed for deleting the user later
     await mongoose.connect(process.env.MONGO_URL);
@@ -14,32 +16,32 @@ test.beforeEach(async ({ page }) => {
 
 test.afterEach(async () => {
     // Delete user created from this test
-    await userModel.deleteMany({email : 'douglas.lim@mail.com'});
+    await userModel.deleteMany({email: emailForDeletion});
     
     // Close the connection with Mongo DB since the test has finished running
     await mongoose.disconnect();
 })
 
 test.describe("Successful Register and Login", () => {
-    test("Should allow the user to register and login successfully", async ({page}) => {
+    // Check that normal user is able to register and login successfully
+    test("Should allow the normal user to register and login successfully", async ({page}) => {
+        // Set email to use for deleting user later
+        emailForDeletion = 'douglas.lim@mail.com';
+
         // Navigating to register page
         await page.getByRole('link', { name: 'Register' }).click();
 
+        // Keying in details under register
         // Filling up the name under register
         await page.getByRole('textbox', { name: 'Enter Your Name' }).fill('Douglas Lim');
-        
         // Filling up the email under register
         await page.getByRole('textbox', { name: 'Enter Your Email' }).fill('douglas.lim@mail.com');
-
         // Filling up the password under register
         await page.getByRole('textbox', { name: 'Enter Your Password' }).fill('simple-password');
-        
         // Filling up the phone number under register
         await page.getByRole('textbox', { name: 'Enter Your Phone' }).fill('91123322');
-        
         // Filling up the address under register
         await page.getByRole('textbox', { name: 'Enter Your Address' }).fill('766 Kent Ridge Road');
-        
         // Filling up the favourite sports under register
         await page.getByRole('textbox', { name: 'What is Your Favorite sports' }).fill('Football');
        
@@ -59,7 +61,7 @@ test.describe("Successful Register and Login", () => {
         // Check that toast message for successful login is shown after login 
         await expect(page.getByText('login successfully')).toBeVisible();
 
-        // Check that correct name appears for user on home page after login
+        // Check that correct name appears for normal user on nav bar of home page after login
         await expect(page.getByRole('list')).toContainText('Douglas Lim');
 
         // Navigate to dashboard
@@ -74,5 +76,53 @@ test.describe("Successful Register and Login", () => {
         await expect(name).toContainText('Douglas Lim');
         await expect(email).toContainText('douglas.lim@mail.com');
         await expect(address).toContainText('766 Kent Ridge Road');
-    })
+    });
+
+    // Check that admin is able to login successfully (Admin is not able to register so we only check login)
+    test('Should allow admin to login successfully', async ({page}) => {
+        // Set email to use for deleting admin later
+        emailForDeletion = "admin.account@mail.com";
+
+        // Create admin for test
+        const hashedPassword = "$2b$10$u/a/pMmAY0Iezeuna3W1OOiggduh3sEla8jhXvg0hUDW6vBIeTeWa";
+
+        const admin = await new userModel({
+            name: "Admin Account", 
+            email: "admin.account@mail.com", 
+            phone: "91123321", 
+            address: "Some Admin Place somewhere", 
+            password: hashedPassword, 
+            answer: "Swimming",
+            role: 1
+        }).save();
+
+        // Navigating to login page
+        await page.getByRole('link', { name: 'Login' }).click();
+
+        // Inputting user details in login page to login
+        await page.getByRole('textbox', { name: 'Enter Your Email' }).fill('admin.account@mail.com');
+        await page.getByRole('textbox', { name: 'Enter Your Password' }).fill('Exact6');
+        
+        // Click on the login button
+        await page.getByRole('button', { name: 'LOGIN' }).click();
+
+        // Check that correct name appears for admin on nav bar of home page after login
+        await expect(page.getByRole('list')).toContainText('Admin Account');
+
+        // Navigate to admin dashboard
+        await page.getByRole('button', { name: 'Admin Account' }).click();
+        await page.getByRole('link', { name: 'Dashboard' }).click();
+
+        // Wait for page to finish loading admin dashboard
+        await page.getByText('Admin Panel').waitFor('visible');
+
+        // Check that admin's name, email and phone number are correct on the dashboard
+        const adminName = page.locator('h3').nth(0);
+        const adminEmail = page.locator('h3').nth(1);
+        const adminPhone = page.locator('h3').nth(2);
+
+        expect(adminName).toContainText(`Admin Name : ${admin.name}`);
+        expect(adminEmail).toContainText(`Admin Email : ${admin.email}`);
+        expect(adminPhone).toContainText(`Admin Contact : ${admin.phone}`);
+    });
 });

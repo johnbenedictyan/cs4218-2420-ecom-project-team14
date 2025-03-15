e4a41fa4a3ff88dba2458be799d1e792a1860aaf
jwdavis0200 <jwdavis0200@gmail.com>
2025-03-15 19:18:25 +0800

    overhaul of AdminRoute to handle sessionStorage properly for admin, non-admin and unauthenticated workflow

--- client/src/components/Routes/AdminRoute.js
+++ client/src/components/Routes/AdminRoute.js
@@ -6,32 +6,62 @@ import { useAuth } from "../../context/auth";
 import Spinner from "../Spinner";
 
 export default function AdminRoute() {
-  const [auth] = useAuth();
+  const [auth, setAuth] = useAuth();
   const token = useMemo(() => auth?.token, [auth]); // Avoid unnecessary re-renders
   const [state, setState] = useState({ loading: true, ok: false });
   const navigate = useNavigate();
 
   useEffect(() => {
     const checkAuth = async () => {
-      if (!token) {
-        navigate("/login");
-        return;
-      }
+      if (!auth?.token) {
+        const storedAuth = localStorage.getItem("auth");
+        if (storedAuth) {
+          const parsedAuth = JSON.parse(storedAuth);
+          setAuth(parsedAuth);
+          const token = parsedAuth.token;
 
-      try {
-        const { data } = await axios.get("/api/v1/auth/admin-auth");
-        setState({ loading: false, ok: data.ok });
-      } catch (error) {
-        console.error("Admin auth check failed:", error);
-        if (error.response.status == 401) {
-          navigate("/forbidden");
-          return;
+          try {
+            const response = await axios.get("/api/v1/auth/admin-auth", {
+              headers: {
+                Authorization: token,
+              },
+            });
+
+            setState({ loading: false, ok: response.data.ok });
+          } catch (error) {
+            if (error.response) {
+              // Failed token authorization check
+              navigate("/forbidden");
+            } else {
+              // Potentially network error
+              navigate("/login");
+            }
+          }
+        } else {
+          // No token in local storage and in auth context
+          navigate("/login");
+        }
+      } else {
+        try {
+          const response = await axios.get("/api/v1/auth/admin-auth", {
+            headers: {
+              Authorization: token,
+            },
+          });
+          setState({ loading: false, ok: response.data.ok });
+        } catch (error) {
+          if (error.response) {
+            // Failed token authorization check
+            navigate("/forbidden");
+          } else {
+            // Potentially network error
+            navigate("/login");
+          }
         }
       }
     };
-
     checkAuth();
-  }, [token]);
+  }, [auth?.token, token, navigate, setAuth]);
 
   return state.loading ? <Spinner /> : <Outlet />;
 }

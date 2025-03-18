3d047147b250b9052b456fdd4a1286f629852729
jwdavis0200 <jwdavis0200@gmail.com>
2025-03-18 14:35:57 +0800

    hotfix for dns resolution issue using full 127.0.0.1 as part of URL

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -20,7 +20,7 @@ const Login = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("/api/v1/auth/login", {
+      const res = await axios.post("http://127.0.0.1:6060/api/v1/auth/login", {
         email,
         password,
       });

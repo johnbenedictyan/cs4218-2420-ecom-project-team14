c2fd82d1e038237bfb242dd626a6c849f40a1a5c
Sampy147 <samuelpangc@gmail.com>
2025-03-18 19:53:04 +0800

    Add update product integration tests and change relevant unit test mocks to account for validation changes

--- server.js
+++ server.js
@@ -37,10 +37,12 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 6060;
 
-app.listen(PORT, () => {
-  console.log(
-    `Server running on ${process.env.DEV_MODE} mode on ${PORT}`.bgCyan.white
-  );
-});
+if (process.env.NODE_ENV !== "test") {
+  app.listen(PORT, () => {
+    console.log(
+      `Server running on ${process.env.DEV_MODE} mode on ${PORT}`.bgCyan.white
+    );
+  });
+}
 
 export default app;

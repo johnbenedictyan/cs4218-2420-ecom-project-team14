3580e3a28036924ad9c7b0b1cb410887d4b0aba4
John Benedict Yan <johnbenedictyan@gmail.com>
2025-03-21 11:46:14 +0800

    set common api url

--- integration-tests/auth/login.test.js
+++ integration-tests/auth/login.test.js
@@ -7,6 +7,7 @@ import app from "../../server.js";
 
 describe("Login Integration Tests", () => {
   let mongoMemoryServer;
+  const apiURL = "/api/v1/auth/login";
 
   // TODO: Make all test parallelisable, check responses
 
@@ -34,7 +35,7 @@ describe("Login Integration Tests", () => {
   // Test 1: Success case where user is able to login successfully with valid email and valid password
   it("should allow the user to login successfully when correct email and password is entered", async () => {
     const response = await request(app)
-      .post("/api/v1/auth/login")
+      .post(apiURL)
       .send(payload)
       .set("Content-Type", "application/json")
       .set("Accept", "application/json");
@@ -59,7 +60,7 @@ describe("Login Integration Tests", () => {
   // Test 2 (Empty email): Case where empty email is inputted
   it("should not allow user with an empty email to login", async () => {
     const response = await request(app)
-      .post("/api/v1/auth/login")
+      .post(apiURL)
       .send(payload)
       .set("Content-Type", "application/json")
       .set("Accept", "application/json");
@@ -77,7 +78,7 @@ describe("Login Integration Tests", () => {
   // Test 3 (Non-empty invalid email): Case where non-empty email is inputted but not found in database
   it("should not allow user with a non-empty email that is not in the database to login", async () => {
     const response = await request(app)
-      .post("/api/v1/auth/login")
+      .post(apiURL)
       .send(payload)
       .set("Content-Type", "application/json")
       .set("Accept", "application/json");
@@ -94,7 +95,7 @@ describe("Login Integration Tests", () => {
   // Test 4 (Empty Password): Case where empty password is inputted
   it("should not allow user with an empty password to login", async () => {
     const response = await request(app)
-      .post("/api/v1/auth/login")
+      .post(apiURL)
       .send(payload)
       .set("Content-Type", "application/json")
       .set("Accept", "application/json");
@@ -112,7 +113,7 @@ describe("Login Integration Tests", () => {
   // Test 5 (Non-empty invalid password): Case where non-empty password is inputted but it is invalid as the hash of the passwords do not match
   it("should not allow user with a non-empty invalid password to login", async () => {
     const response = await request(app)
-      .post("/api/v1/auth/login")
+      .post(apiURL)
       .send(payload)
       .set("Content-Type", "application/json")
       .set("Accept", "application/json");

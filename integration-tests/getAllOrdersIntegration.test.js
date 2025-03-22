07e9282d36b44281cea2f1d5e534c041e4faa7b8
Douglas Lim <douglas.lim.yy@gmail.com>
2025-03-22 23:34:30 +0800

    Made some minor changes to integration test

--- integration-tests/getAllOrdersIntegration.test.js
+++ integration-tests/getAllOrdersIntegration.test.js
@@ -8,131 +8,127 @@ import JWT from "jsonwebtoken";
 import productModel from "../models/productModel";
 import { ObjectId } from "mongodb";
 
-describe("Get All Orders Backend Integration Testing", () => {
-  const hashedPassword =
-    "$2b$10$u/a/pMmAY0Iezeuna3W1OOiggduh3sEla8jhXvg0hUDW6vBIeTeWa";
-  let mongoInMemoryServer, jwtToken;
+describe('Get All Orders Backend Integration Testing', () => {
+    const hashedPassword = "$2b$10$u/a/pMmAY0Iezeuna3W1OOiggduh3sEla8jhXvg0hUDW6vBIeTeWa";
+    let mongoInMemoryServer, jwtToken;
 
-  beforeAll(async () => {
-    // Connecting to in memory mongodb database
-    mongoInMemoryServer = await MongoMemoryServer.create();
-    const uri = mongoInMemoryServer.getUri();
-    await mongoose.connect(uri);
-  });
-
-  beforeEach(async () => {
-    // Creating the admin account (Used later in JWT creation)
-    const admin = await new userModel({
-      name: "Admin Account",
-      email: "admin.account@mail.cpm",
-      phone: "91292838",
-      address: "Admin place somewhere",
-      password: hashedPassword,
-      answer: "Swimming",
-      role: 1,
-    }).save();
-
-    // Creating the JWT Token needed to call the getAllOrdersController
-    jwtToken = await JWT.sign({ _id: admin._id }, process.env.JWT_SECRET, {
-      expiresIn: "7d",
+    beforeAll(async () => {
+        // Connecting to in memory mongodb database
+        mongoInMemoryServer = await MongoMemoryServer.create();
+        const uri = mongoInMemoryServer.getUri();
+        await mongoose.connect(uri);
     });
-  });
 
-  afterAll(async () => {
-    // Disconnecting from database after test has finished
-    await mongoose.disconnect();
-    // Stopping the in memory mongodb server since test has ended
-    await mongoInMemoryServer.stop();
-  });
+    beforeEach(async () => {
+        // Creating the admin account (Used later in JWT creation)
+        const admin = await new userModel({name: "Admin Account",
+            email: "admin.account@mail.cpm",
+            phone: "91292838",
+            address: "Admin place somewhere",
+            password: hashedPassword,
+            answer: "Swimming",
+            role: 1
+        }).save();
 
-  afterEach(async () => {
-    // Clearing the database after each test
-    await orderModel.deleteMany({});
-    await userModel.deleteMany({});
-    await productModel.deleteMany({});
-  });
+        // Creating the JWT Token needed to call the getAllOrdersController
+        jwtToken = await JWT.sign({ _id: admin._id }, process.env.JWT_SECRET, {
+                expiresIn: "7d",
+        });
+    });
 
-  // Test 1: When there are multiple orders, check that all orders are returned and sorted by createdAt
-  it("should return all the orders which are sorted by createdAt when there are multiple orders in the database", async () => {
-    // Creating category id needed for creating products
-    const categoryId = new ObjectId("67d3e462339aeb35c28f1be3");
+    afterAll(async () => {
+        // Disconnecting from database after test has finished
+        await mongoose.disconnect();
+        // Stopping the in memory mongodb server since test has ended
+        await mongoInMemoryServer.stop();
+    });
 
-    // Adding products to database
-    const firstProduct = await new productModel({
-      name: "Mega Lucario EX Trading Card",
-      slug: "mega-lucario-ex-trading-card",
-      description:
-        "This is a trading card for the mega evolved form of lucario",
-      price: 100.5,
-      category: categoryId,
-      quantity: 3,
-      shipping: true,
-    }).save();
+    afterEach(async () => {
+        // Clearing the database after each test
+        await orderModel.deleteMany({});
+        await userModel.deleteMany({});
+        await productModel.deleteMany({});
+    })
 
-    const secondProduct = await new productModel({
-      name: "Hoopa Unbound Trading Card",
-      slug: "hoopa-unbound-trading-card",
-      description:
-        "This is from the Pokemon TCG Shining Legends Expansion Pack",
-      price: 150,
-      category: categoryId,
-      quantity: 2,
-      shipping: false,
-    }).save();
+    // Test 1: When there are multiple orders, check that all orders are returned and sorted by createdAt
+    it('should return all the orders which are sorted by createdAt when there are multiple orders in the database', async () => {
+        // Creating category id needed for creating products
+        const categoryId = new ObjectId("67d3e462339aeb35c28f1be3");
 
-    // Adding buyers to database
-    const firstBuyer = await new userModel({
-      name: "Douglas Lim",
-      email: "douglas.lim@mail.com",
-      phone: "92213141",
-      address: "766 Kent Ridge Road",
-      password: hashedPassword,
-      answer: "Football",
-    }).save();
+        // Adding products to database
+        const firstProduct = await new productModel({
+            name: "Mega Lucario EX Trading Card",
+            slug: "mega-lucario-ex-trading-card",
+            description: "This is a trading card for the mega evolved form of lucario",
+            price: 100.50,
+            category: categoryId,
+            quantity: 3,
+            shipping: true,
+        }).save();
 
-    const secondBuyer = await new userModel({
-      name: "Frank Chan",
-      email: "frank.chan@mail.cpm",
-      phone: "81291919",
-      address: "456 Nanyang Road",
-      password: hashedPassword,
-      answer: "Running",
-    }).save();
+        const secondProduct = await new productModel({
+            name: "Hoopa Unbound Trading Card",
+            slug: "hoopa-unbound-trading-card",
+            description: "This is from the Pokemon TCG Shining Legends Expansion Pack",
+            price: 150,
+            category: categoryId,
+            quantity: 2,
+            shipping: false,
+        }).save();
 
-    // Adding the orders to database
-    await new orderModel({
-      products: [firstProduct._id],
-      buyer: firstBuyer._id,
-    }).save();
+        // Adding buyers to database
+        const firstBuyer = await new userModel({
+            name: "Douglas Lim", 
+            email: "douglas.lim@mail.com", 
+            phone: "92213141", 
+            address: "766 Kent Ridge Road", 
+            password: hashedPassword, 
+            answer: "Football"
+        }).save();
 
-    await new orderModel({
-      products: [secondProduct._id],
-      buyer: secondBuyer._id,
-    }).save();
+        const secondBuyer = await new userModel({
+            name: "Frank Chan",
+            email: "frank.chan@mail.cpm",
+            phone: "81291919",
+            address: "456 Nanyang Road",
+            password: hashedPassword,
+            answer: "Running"
+        }).save();
 
-    const response = await request(app)
-      .get("/api/v1/auth/all-orders")
-      .set("Authorization", jwtToken);
+        // Adding the orders to database
+        await new orderModel({
+            products: [firstProduct._id],
+            buyer: firstBuyer._id,
+        }).save();
 
-    const firstOrder = response.body.orders[0];
-    const secondOrder = response.body.orders[1];
+        await new orderModel({
+            products: [secondProduct._id],
+            buyer: secondBuyer._id
+        }).save();
 
-    // Check that the orders are sorted correctly with the first order being later than the second one
-    expect(new Date(firstOrder.createdAt).getTime()).toBeGreaterThan(
-      new Date(secondOrder.createdAt).getTime()
-    );
-  });
+        const response = await request(app).get('/api/v1/auth/all-orders').set("Authorization", jwtToken);
+        
+        const firstOrder = response.body.orders[0];
+        const secondOrder = response.body.orders[1];
 
-  // Test 2: Check that empty array is returned when there are no orders in the database
-  it("should return an empty array when there are no orders which have been made", async () => {
-    const response = await request(app)
-      .get("/api/v1/auth/all-orders")
-      .set("Authorization", jwtToken);
+        // Check that the orders are sorted correctly with the first order being later than the second one
+        expect(new Date(firstOrder.createdAt).getTime()).toBeGreaterThan(new Date(secondOrder.createdAt).getTime());
 
-    // Check that the response returns an empty array since no orders have been made
+        // Check that the message shows that all orders are fetched successfully
+        expect(response.body["message"]).toBe("Fetched orders successfully");
+        expect(response.body["success"]).toBe(true);
+    });
 
-    expect(response.body["message"]).toBe("Fetched orders successfully");
-    expect(response.body["orders"]).toEqual([]);
-    expect(response.body["success"]).toBe(true);
-  });
-});
+    // Test 2: Check that empty array is returned when there are no orders in the database
+    it('should return an empty array when there are no orders which have been made', async () => {
+        const response = await request(app).get('/api/v1/auth/all-orders').set("Authorization", jwtToken);
+
+        // Check that the response returns an empty array since no orders have been made
+        expect(response.body["orders"]).toEqual([]);
+        
+        // Check that the message shows that all orders are fetched successfully
+        expect(response.body["message"]).toBe("Fetched orders successfully");
+        expect(response.body["success"]).toBe(true);
+    });
+
+})
